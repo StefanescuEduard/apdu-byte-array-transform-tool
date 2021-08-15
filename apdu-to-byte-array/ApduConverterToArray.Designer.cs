9097dda2361a5dc690d83a0a6f016dad76f5e345
Eduard Stefanescu <eduard.stefanescu@nagarro.com>
2021-08-15 19:53:38 +0300

    Added project files.

---
+++ apdu-to-byte-array/ApduConverterToArray.Designer.cs
@@ -0,0 +1,136 @@
+﻿
+namespace ApduToByteArray
+{
+    partial class ApduConverterToArray
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblApdu = new System.Windows.Forms.Label();
+            this.rtbApdu = new System.Windows.Forms.RichTextBox();
+            this.rtbApduArray = new System.Windows.Forms.RichTextBox();
+            this.lblApduArray = new System.Windows.Forms.Label();
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.btnCopy = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblApdu
+            // 
+            this.lblApdu.AutoSize = true;
+            this.lblApdu.Location = new System.Drawing.Point(22, 26);
+            this.lblApdu.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.lblApdu.Name = "lblApdu";
+            this.lblApdu.Size = new System.Drawing.Size(73, 30);
+            this.lblApdu.TabIndex = 1;
+            this.lblApdu.Text = "APDU:";
+            // 
+            // rtbApdu
+            // 
+            this.rtbApdu.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.rtbApdu.Location = new System.Drawing.Point(22, 62);
+            this.rtbApdu.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.rtbApdu.Name = "rtbApdu";
+            this.rtbApdu.Size = new System.Drawing.Size(1326, 248);
+            this.rtbApdu.TabIndex = 2;
+            this.rtbApdu.Text = "";
+            // 
+            // rtbApduArray
+            // 
+            this.rtbApduArray.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.rtbApduArray.Location = new System.Drawing.Point(21, 376);
+            this.rtbApduArray.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.rtbApduArray.Name = "rtbApduArray";
+            this.rtbApduArray.Size = new System.Drawing.Size(1326, 714);
+            this.rtbApduArray.TabIndex = 4;
+            this.rtbApduArray.Text = "";
+            // 
+            // lblApduArray
+            // 
+            this.lblApduArray.AutoSize = true;
+            this.lblApduArray.Location = new System.Drawing.Point(21, 340);
+            this.lblApduArray.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.lblApduArray.Name = "lblApduArray";
+            this.lblApduArray.Size = new System.Drawing.Size(171, 30);
+            this.lblApduArray.TabIndex = 3;
+            this.lblApduArray.Text = "APDU byte array:";
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnConvert.Location = new System.Drawing.Point(1079, 1108);
+            this.btnConvert.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(129, 46);
+            this.btnConvert.TabIndex = 5;
+            this.btnConvert.Text = "Convert";
+            this.btnConvert.UseVisualStyleBackColor = true;
+            this.btnConvert.Click += new System.EventHandler(this.OnButtonConvertClicked);
+            // 
+            // btnCopy
+            // 
+            this.btnCopy.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnCopy.Location = new System.Drawing.Point(1218, 1108);
+            this.btnCopy.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.btnCopy.Name = "btnCopy";
+            this.btnCopy.Size = new System.Drawing.Size(129, 46);
+            this.btnCopy.TabIndex = 6;
+            this.btnCopy.Text = "Copy";
+            this.btnCopy.UseVisualStyleBackColor = true;
+            this.btnCopy.Click += new System.EventHandler(this.OnButtonCopyClicked);
+            // 
+            // ApduConverterToArray
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 30F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1371, 1178);
+            this.Controls.Add(this.btnCopy);
+            this.Controls.Add(this.btnConvert);
+            this.Controls.Add(this.rtbApduArray);
+            this.Controls.Add(this.lblApduArray);
+            this.Controls.Add(this.rtbApdu);
+            this.Controls.Add(this.lblApdu);
+            this.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.Name = "ApduConverterToArray";
+            this.Text = "APDU to APDU byte array";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblApdu;
+        private System.Windows.Forms.RichTextBox rtbApdu;
+        private System.Windows.Forms.RichTextBox rtbApduArray;
+        private System.Windows.Forms.Label lblApduArray;
+        private System.Windows.Forms.Button btnConvert;
+        private System.Windows.Forms.Button btnCopy;
+    }
+}
+
---
+++ apdu-to-byte-array/ApduConverterToArray.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ApduToByteArray
+{
+    public partial class ApduConverterToArray : Form
+    {
+        public ApduConverterToArray()
+        {
+            InitializeComponent();
+        }
+
+        private void OnButtonConvertClicked(object sender, EventArgs e)
+        {
+            rtbApduArray.Clear();
+
+            string apdu = rtbApdu.Text.Trim().Replace(" ", "");
+            StringBuilder apduArray = new();
+            apduArray.AppendLine("var apdu = new byte[]");
+            apduArray.AppendLine("{");
+
+            List<string> apduInParts = SplitInParts(apdu, partLength: 2).ToList();
+
+            int apduInPartLength = apduInParts.Count();
+            for (int indexPart = 0; indexPart < apduInPartLength; indexPart++)
+            {
+                string apduPart = apduInParts[indexPart];
+
+                if (indexPart < apduInPartLength - 1)
+                {
+                    apduPart += ",";
+                }
+
+                apduArray.AppendLine($"\t0x{apduPart}");
+            }
+
+            apduArray.AppendLine("};");
+
+            rtbApduArray.Text = apduArray.ToString();
+        }
+
+        private void OnButtonCopyClicked(object sender, EventArgs e)
+        {
+            Clipboard.SetText(rtbApduArray.Text);
+        }
+
+        private static IEnumerable<string> SplitInParts(string text, int partLength)
+        {
+            for (var i = 0; i < text.Length; i += partLength)
+            {
+                yield return text.Substring(i, Math.Min(partLength, text.Length - i));
+            }
+        }
+    }
+}
